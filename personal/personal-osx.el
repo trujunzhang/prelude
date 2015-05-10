355e0609b0e340113c9cf76dd440e204ebda016c
wanghaogithub720 <wanghaobackup@gmail.com>
2015-05-10 16:04:21 +0800

    add personal-xcode.el

--- personal/personal-osx.el
+++ personal/personal-osx.el
@@ -1,6 +1,9 @@
 ;;(when (eq system-type 'darwin)
   (rvm-use-default)
   (setq mac-option-modifier 'super)
-  (setq mac-command-modifier 'meta)
+(setq mac-command-modifier 'meta)
+
+
+( dired "/Users/djzhang/Desktop/SketchProjects/AsYoutubeiPad-swift-client" )
 
 (provide 'personal-osx)
--- personal/preload/djzhang.el
+++ personal/preload/djzhang.el
@@ -15,7 +15,21 @@
 
 
 ;;; toggle fullscreen
-(toggle-frame-fullscreen)
+;;; (toggle-frame-fullscreen)
+
+
+;;; Disabling whitespace-mode
+
+;;; Although whitespace-mode is awesome some people might find it too intrusive. You can disable it in your personal config with the following bit of code:
+
+(setq prelude-whitespace nil)
+
+;;; If you like whitespace-mode but prefer it to not automatically cleanup your file on save, you can disable that behavior by setting prelude-clean-whitespace-on-save to nil in your config file with:
+
+;;; (setq prelude-clean-whitespace-on-save nil)
+
+;;;; The prelude-clean-whitespace-on-save setting can also be set on a per-file or directory basis by using a file variable or a .dir-locals.el file.
+
 
 
 ;; Show line number
