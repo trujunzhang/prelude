28a2fe34d1128c42816f7ade7312456345453f66
Trujun Zhang <trujunzhang@gmail.com>
2016-02-14 19:03:30 +0800

    add editorconfig.

---
+++ personal/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-layout-name "left7")
+ '(ecb-layout-window-sizes nil)
+ '(ecb-options-version "2.40")
+ '(ecb-primary-secondary-mouse-buttons (quote mouse-1--C-mouse-1))
+ '(ecb-source-path (quote ("d:/myRailsProject" "d:/useful scripts")))
+ '(ecb-tip-of-the-day nil)
+ '(ecb-tree-buffer-style (quote ascii-guides)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
